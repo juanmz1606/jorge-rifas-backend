11062efd5f48b5171340717862b06122764fc992
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2026-03-24 16:14:33 -0500

    fix: CORS dominios produccion Vercel

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'https://jorge-rifas-frontend.vercel.app',
+      'https://jorge-rifas-frontend-58cs8stg6.vercel.app',
+    ],
     credentials: true,
-  });
+  })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
