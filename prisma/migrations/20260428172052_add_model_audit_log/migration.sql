0f0b2215433ce7386fe79c57cf90c67011637ee9
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2026-04-28 12:21:37 -0500

    Tabla de auditoria en bd

---
+++ prisma/migrations/20260428172052_add_model_audit_log/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "AuditLog" (
+    "id" TEXT NOT NULL,
+    "action" TEXT NOT NULL,
+    "entityType" TEXT NOT NULL,
+    "entityId" TEXT NOT NULL,
+    "oldValue" JSONB,
+    "newValue" JSONB,
+    "note" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AuditLog_pkey" PRIMARY KEY ("id")
+);
